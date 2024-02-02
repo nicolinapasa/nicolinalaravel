d4dac90b4efd185f7bb9abc1bd24b83503a6239f
Pasa Nicolina <pasa.nicolina3781@gmail.com>
2024-02-02 14:18:36 +0200

    firstCommit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nicolina's Page</title>
+</head>
+<body>
+    <section class="container">
+        <h1>Pașa Nicolina</h1>
+        <h2>CEITI W-2042</h2>
+    </section>
+</body>
+</html>
