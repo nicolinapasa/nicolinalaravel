3a9970726df10c4d351bb5d45ec73ec9130da31f
Pasa Nicolina <pasa.nicolina3781@gmail.com>
2024-02-06 08:59:55 +0200

    secondCommit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>Nicolina's Page</title>
 </head>
 <body>
-    <section class="container">
+    <section class="container-fluid p-5 text-center">
         <h1>Pașa Nicolina</h1>
         <h2>CEITI W-2042</h2>
+        <img src="/images/photo.jpg" alt="da" class="col-6 rounded mt-3">
     </section>
 </body>
 </html>
